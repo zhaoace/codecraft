692a045153fc44d30ca34405d977e0ac3945fd5f
Zhao, Li <li.zhao02@sap.com>
2014-06-04 17:47:55 +0800

    demo two UT case and TDD to Tony .

--- ruby/learning/unittest/my_first_ut.rb
+++ ruby/learning/unittest/my_first_ut.rb
@@ -1,6 +1,18 @@
 
 
-def outputMyName 
+def outputMyName
     "Zhaoace"
 end
+
+
+
+def helloWorld
+    p "helloWorld , I'm zhaoli "
+end
+
+
+def helloDongdong who
+    "hello, I'm #{who}"
+end
+
 
--- ruby/learning/unittest/test-my_first_ut.rb
+++ ruby/learning/unittest/test-my_first_ut.rb
@@ -6,11 +6,34 @@ require "./my_first_ut.rb"
 
 
 class MyFirstTestPlan < Test::Unit::TestCase
+
+
     def test_outputMyName
         assert_equal('Zhaoace', outputMyName)
-    end    
+    end
 
     def test_inputMyName2
         assert_equal('Zhaoace', outputMyName)
     end
+
+
+
+    def test_helloWorld_no_var
+        result = helloWorld
+        expect = "helloWorld , I'm zhaoli "
+        assert_equal(result, expect)
+    end
+
+
+    def test_helloDongdong_var1
+        var = "dongdong"
+
+        result = helloDongdong var
+        expect = "hello, I'm #{var}"
+
+        assert_equal(result, expect)
+    end
+
+
+
 end
