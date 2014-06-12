6ab393fef880f16b68a987d6b9ce901fa6aafe7a
zhaoli <zhaoace@gmail.com>
2014-06-12 21:55:31 +0800

    ex1 and ex2 from http://learnpythonthehardway.org/book/ex1.html.

---
+++ python/learnpythonthehardway.org/ex1.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+
+
+print "Hello World\n"
+print "Hello Again\n"
+print "I like typing this.\n"
+print "This is fun\n"
+print 'Yay! Printing\n'
+print "I'd much rather you 'not'\n"
+print 'I "said" do not touch this.\n'
+
+
+
---
+++ python/learnpythonthehardway.org/ex2.py
@@ -0,0 +1,10 @@
+# A comment
+# Anything after the #
+
+print "I could have code like this.\n"   #and the comment after is ignored.
+
+
+# you can also use a comment to "disable" or comment out a  piece of code:
+# print "this won't run."
+
+print "this will run."
