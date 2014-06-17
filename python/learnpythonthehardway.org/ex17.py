61f93697e51df6e9d1251fde870b944ff4f62996
Zhao, Li <li.zhao02@sap.com>
2014-06-17 15:43:25 +0800

    add ex to ex19

---
+++ python/learnpythonthehardway.org/ex17.py
@@ -0,0 +1,29 @@
+# encoding: utf-8
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+
+
+# we could to them in one line too.
+input = open(from_file)
+indata = input.read()
+
+
+print " The  input file is %d bytes long " % len(indata)
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN  to continue, CTRL-C to abort."
+raw_input
+
+output = open(to_file, 'w')
+output.write(indata)
+
+
+print "Alright , all done"
+
+
+output.close()
+input.close()
