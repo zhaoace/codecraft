9b456165f28e96d9b51a277a9efc55a1982d3331
Zhao, Li <li.zhao02@sap.com>
2014-06-26 11:47:58 +0800

    Pass the ut and sleep for 1minute.

--- python/projects/autopush_p4/tests/autopush_tests.py
+++ python/projects/autopush_p4/tests/autopush_tests.py
@@ -1,14 +1,17 @@
 from nose.tools import *
 from autopush import autopush
 
+import time
+
 
 
 def test_get_nowtime():
     result = autopush.get_nowtime()
+    time.sleep(60)
     assert result is not None
 
 
-def test_must_fail():
-     assert_equal(False, True)
+# def test_must_fail():
+#     assert_equal(False, True)
 
 
