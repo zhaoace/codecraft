a775a9f568d3ec240e0443891700f5fe619775f8
Zhao, Li <li.zhao02@sap.com>
2014-09-11 16:19:31 +0800

    add a function of replace string with batch.

---
+++ bat/replace_string_in_dos.bat
@@ -0,0 +1,28 @@
+
+echo off
+
+set BASE_STR="c:\anywhere\yes\or\no"
+set TAKEN=o
+set VALUE=bbbb
+Setlocal Enabledelayedexpansion
+for /f "delims=*" %%a in ("%BASE_STR%") do (
+set a=%%a
+set a=!a:%TAKEN%=%VALUE%!
+set x=!a!
+)
+echo !a!
+
+
+REM echo "DEMO for replace \ with / "
+set TAKEN=\
+set VALUE=/
+Setlocal Enabledelayedexpansion
+for /f "delims=*" %%a in ("%BASE_STR%") do (
+set a=%%a
+set a=!a:%TAKEN%=%VALUE%!
+set x=!a!
+)
+echo %x%
+
+
+echo on
